59a875a92b936f2f6b1ef392c9e510441f5614f6
Dornbach Peter <peter@Mac.dornbachs.com>
2026-05-02 23:14:41 +0200

    Add both Bresenham and DDA implementations for comparison.

--- c64o/poly.cc
+++ c64o/poly.cc
@@ -3,8 +3,8 @@
 #include "benchmark.h"
 #include "fmath.h"
 #include "mem.h"
+#include "vec.h"
 #include <stdint.h>
-#include <string.h>
 
 // Buffers to store the left-most and right-most X bounds for each scanline
 static __zeropage uint8_t _min_x[kViewportHeight];
@@ -19,7 +19,76 @@ static inline void _clear_buffers() {
   }
 }
 
-static void _trace_edge(int8_t x1, int8_t y1, int8_t x2, int8_t y2) {
+extern const uint8_t vec_recip_lut[128];
+
+static inline uint16_t _div(uint8_t a, uint8_t b) {
+  if (a == 0) {
+    return 0;
+  }
+
+  uint8_t b_norm = b;
+  uint8_t shift = 0;
+  while (b_norm < 128) {
+    b_norm <<= 1;
+    shift++;
+  }
+  uint16_t b_recip = 0x100 + vec_recip_lut[b_norm - 128];
+  uint16_t p = (uint16_t)a * b_recip;
+  return p >> (8 - shift);
+}
+
+static void _trace_edge_dda(int8_t x1, int8_t y1, int8_t x2, int8_t y2) {
+  if (y1 == y2) {
+    return; // Skip horizontal edges
+  }
+
+  // Enforce top-to-bottom order to guarantee we always step Y positively
+  if (y1 > y2) {
+    int8_t tmp_x = x1;
+    x1 = x2;
+    x2 = tmp_x;
+
+    int8_t tmp_y = y1;
+    y1 = y2;
+    y2 = tmp_y;
+  }
+
+  uint8_t dy = y2 - y1;
+  int8_t dx = x2 - x1;
+  uint8_t abs_dx = _abs8(dx);
+  int16_t dx_fp = _div(abs_dx, dy);
+
+  if (dx < 0) {
+    dx_fp = -dx_fp;
+  }
+
+  int16_t x_fp = ((int16_t)x1 << 8) + 0x80; // +0x80 for half-pixel rounding
+
+  for (int8_t y = y1; y <= y2; y++) {
+    int8_t start_x = x_fp >> 8;
+    int8_t end_x = start_x;
+    if (y < y2) {
+      end_x = (x_fp + dx_fp) >> 8;
+    }
+
+    // Update scanline bounds
+    if (start_x < _min_x[y]) {
+      _min_x[y] = start_x;
+    }
+    if (start_x > _max_x[y]) {
+      _max_x[y] = start_x;
+    }
+    if (end_x < _min_x[y]) {
+      _min_x[y] = end_x;
+    }
+    if (end_x > _max_x[y]) {
+      _max_x[y] = end_x;
+    }
+    x_fp += dx_fp;
+  }
+}
+
+static void _trace_edge_bresenham(int8_t x1, int8_t y1, int8_t x2, int8_t y2) {
   if (y1 == y2) {
     return; // Skip horizontal edges
   }
@@ -107,11 +176,11 @@ void fill_poly(const vertex_t *vertices, uint8_t num_vertices,
   bm_start();
   for (uint8_t i = 0; i < num_vertices; i++) {
     uint8_t next = (i + 1);
-    if (next == num_vertices)
+    if (next == num_vertices) {
       next = 0; // Wrap around to the first vertex
-
-    _trace_edge(vertices[i].x, vertices[i].y, vertices[next].x,
-                vertices[next].y);
+    }
+    _trace_edge_bresenham(vertices[i].x, vertices[i].y, vertices[next].x,
+                          vertices[next].y);
   }
   bm_end(920, SCREEN_STR("trace:"));
 
