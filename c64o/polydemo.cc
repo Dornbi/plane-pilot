b0ad3e765394514560578aaa686299d438c2b645
Dornbach Peter <peter@Mac.dornbachs.com>
2026-05-03 19:59:18 +0200

    More polygons in polydemo.

--- c64o/polydemo.cc
+++ c64o/polydemo.cc
@@ -8,11 +8,10 @@
 #include "mem.h"
 #include <string.h>
 
-static const vertex_t polys[][4] = {{{20, 2}, {35, 8}, {20, 12}, {5, 6}},
-                                    {{0, 0}, {38, 0}, {38, 12}, {0, 12}},
-                                    {{0, 0}, {39, 0}, {39, 13}, {0, 13}},
-                                    {{18, 3}, {22, 3}, {30, 9}, {10, 9}},
-                                    {{10, 5}, {12, 5}, {12, 7}, {10, 7}}};
+static const vertex_t polys[][4] = {
+    {{20, 2}, {35, 8}, {20, 12}, {5, 6}}, {{20, 2}, {24, 6}, {20, 10}, {16, 6}},
+    {{18, 3}, {22, 3}, {30, 9}, {10, 9}}, {{0, 0}, {38, 0}, {38, 12}, {0, 12}},
+    {{0, 0}, {39, 0}, {39, 13}, {0, 13}}, {{10, 5}, {12, 5}, {12, 7}, {10, 7}}};
 
 static void _clear_screen() {
   memset(mem_screen_ram, kCharSolidGround, kViewportHeight * kScreenWidth);
@@ -37,11 +36,13 @@ int main() {
 
   while (1) {
     fill_poly(polys[idx], 4, kQuadCharStart);
+
     keyb_poll();
     if (key_pressed(KSCAN_SPACE)) {
       idx = (idx + 1) % (sizeof(polys) / sizeof(polys[0]));
       _clear_screen();
     }
+
     mem_switch_buffer();
   };
 
